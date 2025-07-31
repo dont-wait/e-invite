34e7d657fe07d4b416254e21ec33f8d61cfad418
dontwait <nguyentansang15042005@gmail.com>
2025-08-01 00:26:09 +0700

    feat: update data and template

--- util.py
+++ util.py
@@ -2,11 +2,22 @@ from datetime import datetime
 
 def format_date(info: dict) -> dict:
     try:
-        dt = datetime.strptime(info["date"], "%Y-%m-%d")
-        info["date_day"] = dt.day
-        info["date_month"] = dt.month
-        info["date_year"] = dt.year
+        write_date = datetime.strptime(info["date_write"], "%Y-%m-%d")
+        meeting_date = datetime.strptime(info["date_meeting"], "%Y-%m-%d")
+
+        if meeting_date < write_date:
+            raise ValueError("Ngày họp không được trước ngày viết thư.")
+
+        info["write_day"] = write_date.day
+        info["write_month"] = write_date.month
+        info["write_year"] = write_date.year
+
+        info["meeting_day"] = meeting_date.day
+        info["meeting_month"] = meeting_date.month
+        info["meeting_year"] = meeting_date.year
+
+        return info
     except Exception as e:
-        raise ValueError(f"Format Error: {e}")
+        raise ValueError(f"Lỗi xử lý ngày: {e}")
 
     return info
